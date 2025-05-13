5f78fc55a97414a6f590413366243d8444672229
prasanna <prasannajaga9@gmail.com>
2025-05-13 08:24:50 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+ 
+import './App.css'   
+import SmoothLiveDndList from './pages/dnd'
+import Products from './pages/products'
+
+function App() { 
+
+  return (
+    <>
+     <main className='flex justify-center'>
+       <Products/>
+       {/* <SmoothLiveDndList/> */}
+     </main>
+   </>
+  )
+}
+
+export default App
